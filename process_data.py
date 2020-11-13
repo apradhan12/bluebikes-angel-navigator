133a26c2dc77eb8ffd1fd04864605bceace243ba
Aaron Pradhan <12a.pradhan@gmail.com>
2020-11-13 10:21:17 -0500

    Create global list of stations across timestamps

--- process_data.py
+++ process_data.py
@@ -3,15 +3,19 @@ import os
 from datetime import datetime
 
 
+overall_stations = {}
+
+
 def process_file_contents(timestamp, contents):
     stations = contents["features"]
     processed = []
-    print(datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S'))
+    # print(datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S'))
     for station in stations:
         coords = station["geometry"]["coordinates"]
         is_active = all(station["properties"]["station"][property_name]
                         for property_name in ["installed", "renting", "returning"])
         station_id = station["properties"]["station"]["id"]
+        name = station["properties"]["station"]["name"]
         if is_active:
             bike_angels_action = station["properties"]["bike_angels_action"]
             if bike_angels_action == "neutral":
@@ -24,8 +28,8 @@ def process_file_contents(timestamp, contents):
                 raise ValueError(f"Bike angels action {bike_angels_action} not recognized for ID {station_id}")
             bikes = station["properties"]["station"]["bikes_available"]
             docks = station["properties"]["station"]["docks_available"]
-            print(f"Bike station {station['properties']['station']['name']} at {coords[0]}, {coords[1]} with id "
-                  f"{station_id} has bike angel points {bike_angels_points}")
+            # print(f"Bike station {name} at {coords[0]}, {coords[1]} with id "
+            #       f"{station_id} has bike angel points {bike_angels_points}")
             processed.append({
                 "coords": coords,
                 "is_active": is_active,
@@ -34,13 +38,27 @@ def process_file_contents(timestamp, contents):
                 "bike_angels_points": bike_angels_points
             })
         else:
-            print(f"Bike station {station['properties']['station']['name']} at {coords[0]}, {coords[1]} with id {station_id} is currently inactive")
+            # print(f"Bike station {name} at {coords[0]}, {coords[1]} with id {station_id} is currently inactive")
             processed.append({
                 "coords": coords,
                 "is_active": is_active,
                 "id": station_id
             })
 
+        station_entry = {
+            "name": name,
+            "coords": coords,
+            "is_active": is_active,
+            "timestamp_added": timestamp
+        }
+
+        if station_id not in overall_stations:
+            overall_stations[station_id] = station_entry
+        else:
+            for field_name in ["name", "coords", "is_active"]:
+                if overall_stations[station_id][field_name] != station_entry[field_name]:
+                    print(f"Station {station_id} at timestamp {timestamp}, field {field_name} doesn't "
+                          f"match earlier value at timestamp {timestamp}")
 
 def main():
     for filename in os.listdir("raw_output/")[0:1]:
