caa28296c2a447081284f60fc2c6075e3d3dc7cc
Aaron Pradhan <12a.pradhan@gmail.com>
2020-11-30 16:46:54 -0500

    Clean up code, add lines for start of day

--- plot_station_fullness.py
+++ plot_station_fullness.py
@@ -1,7 +1,8 @@
 import os
 import json
 import matplotlib.pyplot as plt
-import datetime
+from datetime import datetime
+from datetime import timedelta
 
 ID = "5"
 
@@ -23,54 +24,49 @@ def main():
 
     i = 0
 
-    start_time = None
+    start_date = None
+    end_time = None
 
-    for filename in os.listdir("processed_output/"):
+    filenames = os.listdir("processed_output/")
+    if not filenames:
+        raise Exception("no files found in processed_output/")
+
+    for filename in filenames:
         with open(f"processed_output/{filename}") as file_stream:
             contents = json.load(file_stream)
-            timestamp = int(filename.split(".")[0])
-        if start_time is None:
-            start_time = timestamp
+            timestamp = datetime.fromtimestamp(int(filename.split(".")[0]))
+        if start_date is None:
+            start_date = datetime.combine(timestamp.date(), datetime.min.time())
+        end_time = timestamp  # todo: make this better
         id_contents = process_file_contents(timestamp, contents)
-        timestamps.append(datetime.datetime.fromtimestamp(timestamp))
+        timestamps.append(timestamp)
         valid.append((0, 1, 0) if id_contents[0] else (0, 0.5, 0))
         bikes.append(id_contents[1])
         bikes_and_docks.append(id_contents[1] + id_contents[2])
         capacities.append(id_contents[3])
         points.append(id_contents[4])
         i += 1
-        if i > 500:
-            break
 
-    # timestamps = [ts - start_time for ts in timestamps]
+    end_date = datetime.combine(end_time.date(), datetime.min.time())
 
-    # plt.scatter("timestamps", "bikes", data={"timestamps": timestamps, "bikes": bikes})
+    # TODO: deal with inactive stations
     plt.plot(timestamps, bikes)
     plt.plot(timestamps, bikes_and_docks)
     plt.plot(timestamps, capacities)
-    plt.legend(["# Bikes", "# Bikes + Docks", "Capacity"])
+    plt.plot(timestamps, points)
+
+    while start_date <= end_date:
+        if start_date.weekday() == 0:
+            plt.axvline(x=start_date, c=(0.5, 0.5, 0.5))
+        else:
+            plt.axvline(x=start_date, c=(0.2, 0.2, 0.2))
+        start_date += timedelta(days=1)
+
+    plt.legend(["# Bikes", "# Bikes + Docks", "Capacity", "Angel Points"])
     plt.title(f"Bike capacity at Northeastern University (station ID {ID})")
-    # plt.scatter("timestamps", "bikes_and_docks", c="valid",
-    #             data={"timestamps": timestamps, "bikes_and_docks": bikes_and_docks, "valid": valid})
-    # plt.scatter("timestamps", "capacities", c="valid",
-    #             data={"timestamps": timestamps, "capacities": capacities, "valid": valid})
-    # plt.scatter("timestamps", "points", c="valid",
-    #             data={"timestamps": timestamps, "points": points, "valid": valid})
 
-    # data = {'a': np.arange(50),
-    #         'c': np.random.randint(0, 50, 50),
-    #         'd': np.random.randn(50)}
-    # data['b'] = data['a'] + 10 * np.random.randn(50)
-    # data['d'] = np.abs(data['d']) * 100
-
-    # plt.plot('a', 'b', c='c', s='d', data=data)
-
-    # plt.plot(data["a"], data["b"], 'ro')
-    # plt.plot([1, 2, 3, 4], [1, 4, 9, 16])
-    # plt.axis([0, 6, 0, 20])
-    #
     plt.xlabel("Date/Time")
-    plt.axis(ymin=0)
+    plt.axis(ymin=-3)
 
     plt.show()
 
