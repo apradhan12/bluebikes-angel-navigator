cca07d542d743d9d1902a7eed71bd07a5d89fc11
Aaron Pradhan <12a.pradhan@gmail.com>
2020-12-07 18:07:10 -0500

    Factor out functions from main(), clear plot between frames of animation

--- map_station_trends.py
+++ map_station_trends.py
@@ -14,6 +14,9 @@ import argparse
 UPPER_LEFT_CORNER = (42.4379, -71.3538)
 LOWER_RIGHT_CORNER = (42.2059, -70.8148)
 
+BBOX = (UPPER_LEFT_CORNER[1], LOWER_RIGHT_CORNER[1],
+        LOWER_RIGHT_CORNER[0], UPPER_LEFT_CORNER[0])
+
 RADIUS_MILES = 0.5
 MIN_DIFF_HOUR_BIKES_DELTA = 2.5
 MIN_DIFF_HOUR_POINTS = 1
@@ -52,11 +55,18 @@ def stdev_to_size(avg, stdev):
     return 20
 
 
-def draw(ax, desired_hour, is_weekend, longitudes_list, latitudes_list, colors_list, sizes_list, station_ids_list, lines, stdevs_list):
+def draw(ax, background_img, desired_hour, is_weekend, longitudes_list, latitudes_list, colors_list, sizes_list,
+         station_ids_list, lines, stdevs_list):
     # fig, ax = plt.subplots()
+    plt.cla()
+
+    ax.set_xlim(BBOX[0], BBOX[1])
+    ax.set_ylim(BBOX[2], BBOX[3])
+
+    ax.imshow(background_img, zorder=0, extent=BBOX, aspect="auto")
     ax.scatter(longitudes_list, latitudes_list, zorder=2, alpha=1.0, c=colors_list, s=sizes_list)
-    for i, station_id in enumerate(station_ids_list):
-        ax.annotate(station_id, (longitudes_list[i], latitudes_list[i]), fontsize="xx-small", zorder=3)
+    # for i, station_id in enumerate(station_ids_list):
+    #     ax.annotate(station_id, (longitudes_list[i], latitudes_list[i]), fontsize="xx-small", zorder=3)
     for line in lines:
         plt.plot(line[0], line[1], c="b", zorder=1)
     if SHOW_RATE_OF_CHANGE:
@@ -98,12 +108,7 @@ def calculate_nearby_stations(station_ids_list, latitudes_list, longitudes_list)
     return nearby_stations
 
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-w", "--weekend", help="show weekends", action="store_true")
-    args = parser.parse_args()
-    is_weekend = args.weekend
-
+def read_first_station_status_every_hour():
     # read all the files in the processed_output directory to obtain timeseries data
     filenames = os.listdir("processed_output/")
     if not filenames:
@@ -140,7 +145,10 @@ def main():
                         all_stations[station][timestamp] = contents[station][1]
             previous_date = dt.date()
             previous_hour = dt.hour
+    return all_stations
 
+
+def get_aggregate_station_statistics_by_hour(all_stations, is_weekend):
     all_station_statistics = [{} for _ in range(24)]
     for station in all_stations:
         timestamp_items = list(all_stations[station].items())
@@ -173,65 +181,25 @@ def main():
             stdev = statistics.stdev(hourly_values)
             all_station_statistics[hour][station] = [avg, stdev]
 
-    station_ids_list = sorted(all_station_statistics[0].keys())
-    # create a unified order for the stations
-    station_statistics_list = [[hourly_stats_dict[sid] for sid in station_ids_list]
-                               for hourly_stats_dict in all_station_statistics]
-    """
-    [
-        {"3": [-2.2, 1.25]}, # 0
-        {}, # 1
-        {}, # ...
-        {}  # 23
-    ]
-    [
-        [[-2.2, 1.25], [-1, 0.93]], # 0
-        [], # 1
-        [], # ...
-        []  # 23
-    ]
-    
-    [
-        [-2.2, -1],
-        [],
-        [],
-        []
-    ]
-    """
-    # draw(ax, hour, is_weekend, X longitudes_list, X latitudes_list,
-    #              X colors_list[hour], X sizes_list[hour], station_ids_list,
-    #              lines[hour], stdevs_list[hour])
+    return all_station_statistics
 
-    averages_list = [[station_tuple[0] for station_tuple in hourly_list] for hourly_list in station_statistics_list]
-    stdevs_list = [[station_tuple[1] for station_tuple in hourly_list] for hourly_list in station_statistics_list]
 
-    # print(json.dumps(sorted(
-    #     zip([round(avg, ndigits=2) for avg in averages_list], [round(stdev, ndigits=2) for stdev in stdevs_list],
-    #         station_ids_list))))
-
-    colors_list = [[average_to_color(station_tuple[0]) for station_tuple in hourly_list]
-                   for hourly_list in station_statistics_list]
-    sizes_list = [[stdev_to_size(station_tuple[0], station_tuple[1]) for station_tuple in hourly_list]
-                  for hourly_list in station_statistics_list]
-
-    # get the station coords
-    longitudes_list = []
+def get_station_coords_lists(station_ids_list):
     latitudes_list = []
+    longitudes_list = []
     with open("overall_stations.txt") as file_stream:
         contents = json.load(file_stream)
     for station_id in station_ids_list:
         latitudes_list.append(contents[station_id]["coords"][-1][1][0])
         longitudes_list.append(contents[station_id]["coords"][-1][1][1])
+    return latitudes_list, longitudes_list
 
-    bbox = (UPPER_LEFT_CORNER[1], LOWER_RIGHT_CORNER[1],
-            LOWER_RIGHT_CORNER[0], UPPER_LEFT_CORNER[0])
 
-    boston = plt.imread("map.png")
-
+def calculate_lucrative_station_pairs(station_ids_list, latitudes_list, longitudes_list, averages_list):
     nearby_stations = calculate_nearby_stations(station_ids_list, latitudes_list, longitudes_list)
     lines = [[] for _ in range(24)]
     for hour in range(24):
-        print(f"Drawing lines for hour {hour}")
+        print(f"Finding lines for hour {hour}")
         for i, stations_list in enumerate(nearby_stations):
             for j in stations_list:
                 coords1 = (latitudes_list[i], longitudes_list[i])
@@ -244,15 +212,41 @@ def main():
                     surpasses_min_diff = abs(avg1 - avg2) > MIN_DIFF_HOUR_BIKES_DELTA
                 if surpasses_min_diff:
                     lines[hour].append(([coords1[1], coords2[1]], [coords1[0], coords2[0]]))
+    return lines
 
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-w", "--weekend", help="show weekends", action="store_true")
+    args = parser.parse_args()
+    is_weekend = args.weekend
+
+    # read all the files in the processed_output directory to obtain timeseries data
+    all_stations = read_first_station_status_every_hour()
+    all_station_statistics = get_aggregate_station_statistics_by_hour(all_stations, is_weekend)
+
+    # create a unified order for the stations, then convert dicts into lists to prepare to input into Pyplot
+    station_ids_list = sorted(all_station_statistics[0].keys())
+    station_statistics_list = [[hourly_stats_dict[sid] for sid in station_ids_list]
+                               for hourly_stats_dict in all_station_statistics]
+    averages_list = [[station_tuple[0] for station_tuple in hourly_list] for hourly_list in station_statistics_list]
+    stdevs_list = [[station_tuple[1] for station_tuple in hourly_list] for hourly_list in station_statistics_list]
+    colors_list = [[average_to_color(station_tuple[0]) for station_tuple in hourly_list]
+                   for hourly_list in station_statistics_list]
+    sizes_list = [[stdev_to_size(station_tuple[0], station_tuple[1]) for station_tuple in hourly_list]
+                  for hourly_list in station_statistics_list]
+
+    # get the station coords
+    latitudes_list, longitudes_list = get_station_coords_lists(station_ids_list)
+    # find station pairs
+    lines = calculate_lucrative_station_pairs(station_ids_list, latitudes_list, longitudes_list, averages_list)
+
+    boston = plt.imread("map.png")
     fig, ax = plt.subplots()
 
-    ax.set_xlim(bbox[0], bbox[1])
-    ax.set_ylim(bbox[2], bbox[3])
-    ax.imshow(boston, zorder=0, extent=bbox, aspect="auto")
-
     def build_chart(hr):
-        draw(ax, hr, is_weekend, longitudes_list, latitudes_list,
+        hr = hr % 24
+        draw(ax, boston, hr, is_weekend, longitudes_list, latitudes_list,
              colors_list[hr], sizes_list[hr], station_ids_list,
              lines[hr], stdevs_list[hr])
 
